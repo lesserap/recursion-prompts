193ef997cc52f73a217e7b507955105201ab05fa
Andrew Lesser <lesserap@gmail.com>
2020-11-10 17:45:54 -0700

    Added code for Factorial

--- src/recursion.js
+++ src/recursion.js
@@ -7,6 +7,11 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n > 0) {
+    return n * function(n - 1);
+  } else {
+    return 1;
+  }
 };
 
 // 2. Compute the sum of an array of integers.
