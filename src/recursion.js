fa4ac134df8fbe85204aad2f81bc8b3df7cdcd3e
Andrew Lesser <lesserap@gmail.com>
2020-11-11 00:09:24 -0700

    Code for Sum Integers Array

--- src/recursion.js
+++ src/recursion.js
@@ -7,8 +7,10 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
-  if (n > 0) {
-    return n * function(n - 1);
+  if (n < 0) {
+    return null;
+  } else if (n > 0) {
+    return n * factorial(n - 1);
   } else {
     return 1;
   }
@@ -17,11 +19,25 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+   return array[0] + sum(array.slice(1));
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  array.forEach(function(item){
+    if (!Array.isArray(item)) {
+      sum += item;
+    } else {
+      sum += arraySum(item);
+    }
+  });
+  return sum;
 };
 
 // 4. Check if a number is even.
